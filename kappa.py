5dbaf0eaf2b905d6c0fb51159dedc5f2da617f11
Ahmad Qadri <arqchicago@gmail.com>
2022-07-13 23:35:26 -0400

    added a basic bollinger band

--- kappa.py
+++ kappa.py
@@ -1,5 +1,6 @@
 from data_proc import data_load
 from technicals import simple_technicals
+from visualizer import arq_viz    
 import datetime as dt
 
 if __name__ == '__main__':
@@ -16,16 +17,54 @@ if __name__ == '__main__':
     
     # adding in more variables
     sp500_df['quarter'] = sp500_df['Date'].dt.quarter
+
+
+    # adding dependent variables
+    # ----------------------------------------------------------------
+
+    # 30 day return (%)
+    sp500_df['ret_30day'] = sp500_df['Close'].pct_change(30)
+    sp500_df['ret_30day'] = sp500_df['ret_30day'].shift(-30)  #.fillna(method='ffill')
+
+    # 60 day return (%)    
+    sp500_df['ret_60day'] = sp500_df['Close'].pct_change(60)
+    sp500_df['ret_60day'] = sp500_df['ret_60day'].shift(-60)  #.fillna(method='ffill')
+
+    # 90 day return (%)
     sp500_df['ret_90day'] = sp500_df['Close'].pct_change(90)
-    sp500_df['ret_180day'] = sp500_df['Close'].pct_change(180)
-    sp500_df['ret_270day'] = sp500_df['Close'].pct_change(270)
-    sp500_df['ret_1yr'] = sp500_df['Close'].pct_change(365)
-    
+    sp500_df['ret_90day'] = sp500_df['ret_90day'].shift(-90)  #.fillna(method='ffill')
+    #sp500_df['ret_1yr'] = sp500_df['Close'].pct_change(365)
+
+
+    # adding simple technicals as features
+    # ----------------------------------------------------------------    
     sp500_techs = simple_technicals(sp500_df)
-    sp500_techs.moving_avg(5, 'Close', 'ma_5')
-    sp500_techs.moving_avg(200, 'Close', 'ma_200')
-    sp500_df = sp500_techs.get_df()
     
-    print(sp500_df.head(250).to_string())
-    print(sp500_df.to_string())
+    # 5 day moving avg and moving std deviation
+    sp500_techs.moving_avg(5, 'Close', 'mavg_5')
+    sp500_techs.moving_std(5, 'Close', 'mstd_5')
+    sp500_techs.bol_bands(5, 2, 'mavg_5', 'mstd_5', 'lbb_5', 'ubb_5')
+
+    # 10 day moving avg and moving std deviation
+    sp500_techs.moving_avg(10, 'Close', 'mavg_10')
+    sp500_techs.moving_std(10, 'Close', 'mstd_10')
+    
+    sp500_techs.moving_avg(200, 'Close', 'mavg_200')
+    sp500_df = sp500_techs.get_df()
+    #sp500_df['mavg10_mstd_10'] = sp500_df['mavg_10'] - sp500_df['mstd_10']
+    
+    print(sp500_df.head(225).to_string())
+    print(sp500_df)
+    #print(sp500_df.to_string())
     print(sp500_df.columns.tolist())
+    
+
+    # visualize
+    # pick a timeframe to visualize (or create the plot for all time units)
+    mask = (sp500_df['Date'] > '2005-06-01') & (sp500_df['Date'] <= '2022-06-25')
+
+    line_chart = arq_viz(sp500_df.loc[mask])
+    line_chart.plot_title = 'SP500'
+    line_chart.plot_suptitle = 'Moving Averages'
+    line_chart.watermark = 'draft'
+    line_chart.line_chart('line_chart_sp500.png', 'Date', ['Close', 'mavg_200'], watermark_loc='lower_left')
--- technicals.py
+++ technicals.py
@@ -15,6 +15,14 @@ class simple_technicals:
     def moving_avg(self, n, colname, new_colname):
         self.df[new_colname] = self.df[colname].rolling(n).mean().fillna(method='bfill')
 
+    def moving_std(self, n, colname, new_colname):
+        self.df[new_colname] = self.df[colname].rolling(n).std().fillna(method='bfill')
+
+    def bol_bands(self, n, m, moving_avg_colname, moving_std_colname, new_colname_lbb, new_colname_ubb):
+        self.df[new_colname_lbb] = self.df[moving_avg_colname].rolling(n).std().fillna(method='bfill') - m * self.df[moving_std_colname].rolling(n).std().fillna(method='bfill')
+        self.df[new_colname_ubb] = self.df[moving_avg_colname].rolling(n).std().fillna(method='bfill') + m * self.df[moving_std_colname].rolling(n).std().fillna(method='bfill')
+        
+
 
 if __name__ == '__main__':
 
